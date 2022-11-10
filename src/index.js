b0ac7a9ff0ccbd155c37f3c62145a2f22b26b73e
sjortiz <sortizjim@gmail.com>
2022-11-10 12:28:05 -0400

    converting optiuons to spanish and adding two more events

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
 
+const lang = {
+  es: {
+    week: 'Semana',
+    work_week: 'Semana de trabajo',
+    day: 'Día',
+    month: 'Mes',
+    previous: 'Atrás',
+    next: 'Después',
+    today: 'Hoy',
+    agenda: 'Agenda',
+
+    showMore: (total) => `+${total} más`,
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -18,6 +33,27 @@ class App extends Component {
         allDay: false,
         start: new Date(2022, 11-1, 13, 9, 30, 0),
         end: new Date(2022, 11-1, 13, 18, 0, 0),
+      },
+      {
+        id: 2,
+        title: 'Rally cacata',
+        allDay: false,
+        start: new Date(2022, 11-1, 19, 8, 0, 0),
+        end: new Date(2022, 11-1, 20, 12, 0, 0),
+      },
+      {
+        id: 3,
+        title: 'la vega - 2do monteo invitacional',
+        start: new Date(2022, 11-1, 13, 9, 0, 0),
+        end: new Date(2022, 11-1, 13, 16, 0, 0),
+
+      },
+      {
+        id: 4,
+        title: 'Rally constitucion',
+        start: new Date(2022, 11-1, 12, 9, 0, 0),
+        end: new Date(2022, 11-1, 12, 16, 0, 0),
+
       }
     ]
     this.state = {
@@ -30,13 +66,15 @@ class App extends Component {
     return (
       <div>
         <p>
-          A test for the React Big Calendar.
+          Calendario de monteo. 
+          <b>Para agregar eventos contactar al team bronco</b>
         </p>
         <div style={{ height: '500pt'}}>
           <Calendar
             events={this.state.events}
             startAccessor="start"
             endAccessor="end"
+            messages={lang['es']}
             defaultDate={moment().toDate()}
             localizer={localizer}
           />
